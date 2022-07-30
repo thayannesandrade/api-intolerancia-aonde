6cdeb83e45ce9e3300c255ed1839269c880b15e9
Thayanne Andrade <thayannesandrade@gmail.com>
2022-07-30 02:50:57 -0300

    adjust index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-const port = Process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 // forma de ler json
